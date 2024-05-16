7b1184b1d46fd12af6b5d40b26427e06411cd5b9
Jae Hun Lee <118325810+importantsgit@users.noreply.github.com>
2024-05-17 01:08:59 +0900

    [이재훈] TTSManager -> TextSpeaker 네이밍 변경 / 싱글톤 해제 / actor 로 전환
    수정사항
    - TextSpeaker 싱글톤에 의존하는 문제로 제거
    - actor로 전환 ( race condition down )

--- SpeechRecognition/SpeechRecognition/Service/TextSpeaker.swift
+++ SpeechRecognition/SpeechRecognition/Service/TextSpeaker.swift
@@ -8,99 +8,57 @@
 import Foundation
 import AVFAudio
 
-class TTSManager:NSObject {
-    
-    static let shared = TTSManager()
+// Text To Speak
+actor TextSpeaker: NSObject {
 
-    
     private let synthesizer = AVSpeechSynthesizer()
-    
-    private let voice = Voices.siri_Aaron
+    private var utteranceSetting: CustomUtterance!
     
     enum TTSError: Error {
         case TTSUnavailable
+        case TextEmpty
         
         var message: String {
             switch self {
             case .TTSUnavailable: return "TTS is unvailable"
+            case .TextEmpty: return "Text is not existed"
             }
         }
-    }
-
-    enum Voices {
-        // 할아버지?
-        static let Grandpa = "com.apple.eloquence.en-GB.Grandpa"
-        
-        // 할머니?
-        static let Grandma = "com.apple.eloquence.en-US.Grandma"
-        
-        // 살찐 남자
-        static let Eddy = "com.apple.eloquence.en-US.Eddy"
-        
-        // 영국 eddy
-        static let GBEddy = "com.apple.eloquence.en-GB.Eddy"
-        
-        // 살찐 남자
-        static let Reed = "com.apple.eloquence.en-US.Reed"
-        
-        // 시리
-        static let siri_Nicky = "com.apple.ttsbundle.siri_Nicky_en-US_compact"
-        
-        static let siri_Aaron = "com.apple.ttsbundle.siri_Aaron_en-US_compact"
-        
-        // 어린아이
-        static let Princess = "com.apple.speech.synthesis.voice.Princess"
-        
-        // 어린아이
-        static let Flo = "com.apple.eloquence.en-US.Flo"
-        
-        // 아주 낮은 목소리를 가진 남자
-        static let Rocko = "com.apple.eloquence.en-US.Rocko"
-        
-        // 여자
-        static let Kathy = "com.apple.speech.synthesis.voice.Kathy"
-        
-        // 남자
-        static let Ralph = "com.apple.speech.synthesis.voice.Ralph"
-        
-        // kr
-        static let Yuna = "com.apple.voice.compact.ko-KR.Yuna"
-        
-        static let song = "com.apple.speech.synthesis.voice.Bubbles]+com.apple.speech.synthesis.voice.Bubbles"
     }
     
     var stoppedSpeak: ((Bool)->Void)?
     
-    override init() {
+    init(utterance: CustomUtterance = .init()) {
         super.init()
         synthesizer.delegate = self
-    }
-    
-    func checkVoices() {
-        AVSpeechSynthesisVoice.speechVoices().forEach{
-            print("\($0.description)+\($0.identifier)")
+        
+        Task {
+            await self.setUtterance(utterance)
+            try await self.prepareAudioSession()
         }
-        print("------")
     }
     
-    func play(_ text: String?) {
+    private func setUtterance(_ utterance: CustomUtterance) async {
+        self.utteranceSetting = utterance
+    }
+    
+    public func changeUtterance(_ utterance: CustomUtterance) {
+        self.utteranceSetting = utterance
+    }
+    
+    func play(_ text: String?) throws {
         guard let text = text else {
-            print(text!)
-            
-            return
-        }
-        do {
-            try prepareAudioSession()
-        } catch {
-            transcribe(error)
+            throw TTSError.TextEmpty
         }
        
         let utterance = AVSpeechUtterance(string: text)
         // 음성 합성을 위한 텍스트와 음성에 영향을 미치는 매개변수를 캡슐화하는 개체
         // 여러 utterance로 나누어 다른 말투로 말하게 할 수 있음
         
-        //utterance.voice = AVSpeechSynthesisVoice(identifier: voice)
-        utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+        utterance.voice = AVSpeechSynthesisVoice(
+            identifier: utteranceSetting.voice.rawValue
+        )
+        //utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
         // en-KR 가 아니면 오류 생김
         //AVSpeechSynthesisVoice(language: "en-KR")
         // 음성 합성기가 발화할 때 사용하는 음성
@@ -108,19 +66,19 @@ class TTSManager:NSObject {
         
         // utterance를 enqueue(queue)하기 전에 이 속성들을 사용해야 함
         
-        utterance.rate = 0.50
+        utterance.rate = utteranceSetting.rate
         // 말하기 속도
         // AVSpeechUtteranceMinimumSpeechRate ~ AVSpeechUtteranceMaximumSpeechRate 범위 내의 소수 값
         // 기본값: AVSpeechUtteranceDefaultSpeechRate
         
-        utterance.pitchMultiplier = 1.0
+        utterance.pitchMultiplier = utteranceSetting.pitchMultiplier
         // 발화할 때 음성 합성기가 사용하는 기준 피치 (음성의 높고 낮음을 결정하는 인식된 주파수)
         // 0.5~2.0 기본값: 1.0
         
-        utterance.postUtteranceDelay = 1.0
+        utterance.postUtteranceDelay = utteranceSetting.postUtteranceDelay
         // 여러 utterance가 대기열에 존재할 때, 음성 합성기는 현재 utterance의 postUtteranceDelay와 다음 utterance의 preUtteranceDelay의 합과 동일한 최소 시간만큼 일시적으로 일시정지함
         
-        utterance.volume = 1.0
+        utterance.volume = utteranceSetting.volume
         // 0.0~1.0 기본값: 1.0
         
         synthesizer.stopSpeaking(at: .immediate)
@@ -159,27 +117,27 @@ class TTSManager:NSObject {
 
 // 각 의미있는 단위마다 utterance를 생성하여 음성 진행 상황에 대한 알림을 받고 싶으면 delegate 채택
 
-extension TTSManager: AVSpeechSynthesizerDelegate {
-    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didPause utterance: AVSpeechUtterance) {
+extension TextSpeaker: AVSpeechSynthesizerDelegate {
+    private func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didPause utterance: AVSpeechUtterance) async {
         //
     }
     
-    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didStart utterance: AVSpeechUtterance) {
+    private func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didStart utterance: AVSpeechUtterance) async {
         //
     }
     
-    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didCancel utterance: AVSpeechUtterance) {
+    private func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didCancel utterance: AVSpeechUtterance) async {
         //
     }
     
-    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
+    private func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) async {
     }
     
-    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didContinue utterance: AVSpeechUtterance) {
+    private func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didContinue utterance: AVSpeechUtterance) async {
         //
     }
     
-    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, willSpeakRangeOfSpeechString characterRange: NSRange, utterance: AVSpeechUtterance) {
+    private func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, willSpeakRangeOfSpeechString characterRange: NSRange, utterance: AVSpeechUtterance) async {
         //
     }
     
